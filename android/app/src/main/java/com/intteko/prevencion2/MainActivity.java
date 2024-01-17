f36d19667d561c8b603a9d4a17af2c28f09a448a
alemartin80 <alemartin80@gmail.com>
2024-01-17 11:26:16 +0100

    commit

---
+++ android/app/src/main/java/com/intteko/prevencion2/MainActivity.java
@@ -0,0 +1,5 @@
+package com.intteko.prevencion2;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.intteko.prevencion2',
+  appName: 'iCheckList',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
